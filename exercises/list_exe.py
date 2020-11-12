a659f9f8dd7ae694958157628beda3c47f9e8725
zhoucong <505614943@qq.com>
2020-11-12 17:10:14 +0800

    py代码

--- exercises/list_exe.py
+++ exercises/list_exe.py
@@ -4,12 +4,21 @@
 # a = list1.pop(2)  # pop()获取并删除指定位置的元素
 # print(a, list1)
 
+
 # list2 = [1,2,3,4,"a","b","c"]
 # a = list2.index("a") #index()查询具有特定值的元素位置
 # list2.insert(1,"d") #insert()指定位置插入元素
 # list2.append("e") #append()添加元素至尾部
 # list2.extend(["he","bing"]) #extend()合并列表至尾部，使用+=也能合并列表
 # print(a,list2)
+
+
+# list3=[4,3,4,5,2,1]
+# a = list3.count(4)#count()记录指定元素出现的次数
+# list3.sort()#sort()对列表进行升序排序
+# list3.reverse()#reverse()对列表进行降序排序
+# b = len(list3)#len()获取长度
+# print(a,b,list3)
 
 
 
