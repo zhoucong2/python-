cd336d00a138e25b737ad1b5e31040f683825e5f
zhoucong <505614943@qq.com>
2020-11-13 15:49:23 +0800

    py代码

--- exercises/7.code_stucture.py
+++ exercises/7.code_stucture.py
@@ -8,5 +8,13 @@ list1=[(m, n) for m,n in e2f.items() ]
 list2=[(m,n) for m in e2f.keys() for n in e2f.values() ] #列表推导式中的嵌套子循环
 print(list1,"\n",list2)
 
-
 """字典推导式"""
+word = "letters"
+letter_count = {letter:word.count(letter) for  letter in word}
+print(letter_count)
+
+"""集合推导式"""
+a_set = {number for number in range(6) if number % 2 ==0}
+print(a_set)
+
+
